09c408c4671ba7419ffdb73a1429fe3bd11e9d7e
Costan Emilian-Mihai <rollys012@gmail.com>
2026-04-18 10:16:54 +0000

    i2

--- main.tf
+++ main.tf
@@ -371,7 +371,7 @@ resource "aws_instance" "observability" {
   private_ip             = "10.0.0.20"
 
   root_block_device {
-    volume_size           = 20
+    volume_size           = 30
     volume_type           = "gp3"
     delete_on_termination = true
     encrypted             = true
